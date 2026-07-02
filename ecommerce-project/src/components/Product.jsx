07cf1026a55e60432e506b38e9773a4ff442b813
Paul Welby <c205428@ardentmills.com>
2026-07-01 21:09:45 -0700

    Move the HomePage into React, with all the images as well

---
+++ ecommerce-project/src/components/Product.jsx
@@ -0,0 +1,57 @@
+export function Product({ product }) {
+  const ratingImageUrl = `images/ratings/rating-${product.rating.stars * 10}.png`;
+  const price = (product.priceCents / 100).toFixed(2);
+
+  return (
+    <div className="product-container">
+      <div className="product-image-container">
+        <img className="product-image" src={product.image} alt={product.name} />
+      </div>
+
+      <div className="product-name limit-text-to-2-lines">
+        {product.name}
+      </div>
+
+      <div className="product-rating-container">
+        <img
+          className="product-rating-stars"
+          src={ratingImageUrl}
+          alt={`${product.rating.stars} stars`}
+        />
+        <div className="product-rating-count link-primary">
+          {product.rating.count}
+        </div>
+      </div>
+
+      <div className="product-price">
+        ${price}
+      </div>
+
+      <div className="product-quantity-container">
+        <select defaultValue="1">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div className="product-spacer"></div>
+
+      <div className="added-to-cart">
+        <img src="images/icons/checkmark.png" alt="" />
+        Added
+      </div>
+
+      <button className="add-to-cart-button button-primary">
+        Add to Cart
+      </button>
+    </div>
+  );
+}
---
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -0,0 +1,47 @@
+import './HomePage.css'
+import './header.css'
+import { products } from '../data/products'
+import { Product } from '../components/Product'
+
+export function HomePage() {
+  return (
+    <>
+      <div className="header">
+        <div className="left-section">
+          <a href="/" className="header-link">
+            <img className="logo" src="images/logo-white.png" alt="SuperSimpleDev" />
+            <img className="mobile-logo" src="images/mobile-logo-white.png" alt="SuperSimpleDev" />
+          </a>
+        </div>
+
+        <div className="middle-section">
+          <input className="search-bar" type="text" placeholder="Search" />
+
+          <button className="search-button">
+            <img className="search-icon" src="images/icons/search-icon.png" alt="Search" />
+          </button>
+        </div>
+
+        <div className="right-section">
+          <a className="orders-link header-link" href="orders.html">
+            <span className="orders-text">Orders</span>
+          </a>
+
+          <a className="cart-link header-link" href="checkout.html">
+            <img className="cart-icon" src="images/icons/cart-icon.png" alt="Cart" />
+            <div className="cart-quantity">3</div>
+            <div className="cart-text">Cart</div>
+          </a>
+        </div>
+      </div>
+
+      <div className="home-page">
+        <div className="products-grid">
+          {products.map((product) => (
+            <Product product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
